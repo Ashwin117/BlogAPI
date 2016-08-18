851ca3c9d9e5242217bfb48af5b241c5338d5bca
Ashwin Sundar <Ashwin117@github.com>
2016-08-17 19:33:24 -0700

    Added authentication

--- controllers/actions.js
+++ controllers/actions.js
@@ -3,21 +3,20 @@ const mongojs = require('mongojs');
 const db = mongojs('blog', ['posts']);
 
 module.exports = {
-	insertPost(req, res) {
+	insertPost(req) {
 		return new Promise ((resolve, reject) => {
 			req.body.comments = [];
 			db.posts.insert(req.body, (err, doc) => {
 				if (err || !doc) {
 					err = err || 500;
 					reject(err);
-				} else {
-					console.log(`Adding blog post...`);
-					resolve(doc);
 				}
+				console.log(`Adding blog post...`);
+				resolve(doc);
 			});
 		})
 	},
-	retrievePost(req, res) {
+	retrievePost(req) {
 		return new Promise ((resolve, reject) => {
 			db.posts.findOne({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
 				if (err || !doc) {
@@ -29,7 +28,7 @@ module.exports = {
 			});
 		})
 	},
-	modifyPost(req, res, modBody) {
+	modifyPost(req, modBody) {
 		return new Promise ((resolve, reject) => {
 			db.posts.findAndModify({query: {_id: mongojs.ObjectId(req.params.id)},
 				update:{$set: modBody},
@@ -38,23 +37,37 @@ module.exports = {
 				if (err || !doc) {
 					err = err || 500;
 					reject(err);
-				} else {
-					console.log(`Updating blog post with id ${req.params.id}...`);
-					resolve(doc);
 				}
+				console.log(`Updating blog post with id ${req.params.id}...`);
+				resolve(doc);
 			});
 		})
 	},
-	deletePost(req, res) {
+	deletePost(req) {
 		return new Promise ((resolve, reject) => {
 			db.posts.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
 				if (err || !doc) {
 					err = err || 500;
 					reject(err);
-				} else {
-					console.log(`Removing blog post with id ${req.params.id}...`);
-					resolve(doc);
 				}
+				console.log(`Removing blog post with id ${req.params.id}...`);
+				resolve(doc);
+			});
+		})
+	},
+	checkUser(req) {
+		return new Promise ((resolve, reject) => {
+			db.posts.find({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+				debugger;
+				if (err || !doc) {
+					err = err || 500;
+					reject(err);
+				}
+				if (!req.params.commentid && req.user.username !== doc[0].username ||
+					req.params.commentid && req.user.username !== doc[0].comments[req.params.commentid].username) {
+					reject('Invalid user');
+				}
+				resolve();
 			});
 		})
 	}
--- index.js
+++ index.js
@@ -3,74 +3,104 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const stormpath = require('express-stormpath');
 const actions = require('./controllers/actions');
 const utils = require('./controllers/utils');
 
 const app = express();
 const PORT = 8080;
 
+app.use(stormpath.init(app, {
+  	expand: {
+    	customData: true,
+  	},
+  	web: {
+		produces: ['application/json']
+	}
+}));
 app.use(morgan('combined'));
 app.use(bodyParser());
 
 console.log(`Listening on port:${PORT}`);
 app.listen(PORT);
 
-app.post('/posts/', (req, res) => {
+app.post('/posts/', stormpath.apiAuthenticationRequired, (req, res) => {
 	utils.checkForProperty(req.body.message);
-	actions.insertPost(req, res)
+	req.body.username = req.user && req.user.username;
+	actions.insertPost(req)
 	.then(handleResponse(res))
 	.catch(handleErrorResponse(res))
 });
 
 app.get('/posts/:id', (req, res) => {
-	actions.etrievePost(req, res)
+	actions.retrievePost(req)
 	.then(handleResponse(res))
 	.catch(handleErrorResponse(res))
 });
 
-app.put('/posts/:id', (req, res) => {
-	actions.modifyPost(req, res, req.body)
-	.then(handleResponse(res))
-	.catch(handleErrorResponse(res))
-});
-
-app.delete('/posts/:id', (req, res) => {
-	actions.deletePost(req)
-	.then(handleResponse(res))
-	.catch(handleErrorResponse(res))
-});
-
-app.post('/posts/:id/comments', (req, res) => {
+app.put('/posts/:id', stormpath.apiAuthenticationRequired, (req, res) => {
 	utils.checkForProperty(req.body.message);
+	req.body.username = req.user && req.user.username;
+	actions.checkUser(req)
+	.then(() => {
+		actions.modifyPost(req, req.body)
+		.then(handleResponse(res))
+		.catch(handleErrorResponse(res))
+	})
+	.catch(handleErrorResponse(res))
+});
+
+app.delete('/posts/:id', stormpath.apiAuthenticationRequired, (req, res) => {
+	actions.checkUser(req)
+	.then(() => {
+		actions.deletePost(req)
+		.then(handleResponse(res))
+		.catch(handleErrorResponse(res))
+	})
+	.catch(handleErrorResponse(res))
+});
+
+app.post('/posts/:id/comments', stormpath.apiAuthenticationRequired, (req, res) => {
+	utils.checkForProperty(req.body.message);
+	req.body.username = req.user && req.user.username;
 	actions.retrievePost(req)
 	.then((doc) => {
 		req.body.id = doc.comments.length;
 		doc.comments.push(req.body);
-		actions.modifyPost(req, res, doc)
+		actions.modifyPost(req, doc)
 		.then(handleResponse(res))
 		.catch(handleErrorResponse(res))
 	})
 	.catch(handleErrorResponse(res))
 });
 
-app.put('/posts/:id/comments/:commentid', (req, res) => {
-	actions.retrievePost(req, res)
-	.then((doc) => {
-		doc.comments[req.params.commentid].message = req.body.message
-		actions.modifyPost(req, res, doc)
-		.then(handleResponse(res))
+app.put('/posts/:id/comments/:commentid', stormpath.apiAuthenticationRequired, (req, res) => {
+	utils.checkForProperty(req.body.message);
+	actions.checkUser(req)
+	.then(() => {
+		actions.retrievePost(req)
+		.then((doc) => {
+			doc.comments[req.params.commentid].message = req.body.message
+			actions.modifyPost(req, doc)
+			.then(handleResponse(res))
+			.catch(handleErrorResponse(res))
+		})
 		.catch(handleErrorResponse(res))
 	})
 	.catch(handleErrorResponse(res))
 });
 
-app.delete('/posts/:id/comments/:commentid', (req, res) => {
-	actions.retrievePost(req, res)
-	.then((doc) => {
-		utils.reSizeIds(doc.comments, parseInt(req.params.commentid)+1);
-		doc.comments.splice(req.params.commentid, 1);
-		actions.modifyPost(req, res, doc)
-		.then(handleResponse(res))
+app.delete('/posts/:id/comments/:commentid', stormpath.apiAuthenticationRequired, (req, res) => {
+	actions.checkUser(req)
+	.then(() => {
+		actions.retrievePost(req)
+		.then((doc) => {
+			utils.reSizeIds(doc.comments, parseInt(req.params.commentid)+1);
+			doc.comments.splice(req.params.commentid, 1);
+			actions.modifyPost(req, doc)
+			.then(handleResponse(res))
+			.catch(handleErrorResponse(res))
+		})
 		.catch(handleErrorResponse(res))
 	})
 	.catch(handleErrorResponse(res))
@@ -78,7 +108,7 @@ app.delete('/posts/:id/comments/:commentid', (req, res) => {
 
 function handleResponse (res) {
 	return ((doc) => {
-		res.json(doc)
+		res.json(doc);
 	});
 }
 
